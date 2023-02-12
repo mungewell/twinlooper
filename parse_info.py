c3fe4e9b285982ccde587fa06cd3ca77e1304f06
Simon Wood <simon@mungewell.org>
2023-02-12 13:57:08 -0700

    Minor corrects - remove 3rd block processing

--- parse_info.py
+++ parse_info.py
@@ -27,6 +27,7 @@ while True:
         break
     else:
         base.append((info[address]) + (info[address+1] * 256))
+        #base.append((info[address+1]) + (info[address] * 256))
         address += 2
 
 sbase = set(base)
@@ -41,6 +42,7 @@ overdub = []
 address = 0x0524
 for i in range(len(base)):
     overdub.append((info[address]) + (info[address+1] * 256))
+    #overdub.append((info[address+1]) + (info[address] * 256))
     address += 2
 
 #print(hexdump(info[address-32:address+32]))
@@ -58,45 +60,14 @@ print("Unique values: ", len(list(stotal)))
 print("")
 
 
-# Third section - it is not clear what this is for, probably doesn't
-# hold enough information for a complete list. 
-
-# Check TShark logs to confirm we download all of it...
-'''
-00000a30  00 00 00 00 00 00 00 00  6c 6f 6f 70 65 72 00 01  |........looper..|
-00000a40  00 f7 fd ff b2 fe ff ba  fb ff 76 f6 ff 44 00 00  |..........v..D..|
-00000a50  79 f7 ff 74 01 00 fb fd  ff cb 00 00 0f 00 00 8e  |y..t............|
-...
-00000bb0  ff ff b6 07 00 51 02 00  af ff ff e0 03 00 50 fe  |.....Q........P.|
-00000bc0  ff af 04 00 74 fe ff d3  01 00 96 fb ff 9b fb ff  |....t...........|
-00000bd0  c0 fb ff                                          |...|
-00000bd3
-'''
-
-index = []
-address = 0x0A3F        # through to 0x0bd3, 404 bytes
-
-'''
-for i in range(128):
-    index.append((info[address]) + (info[address+1] * 256))
-    address += 2
-for i in range(128):
-    index.append((info[address+2]) + (info[address+1] * 256) + \
-            (info[address] * 256 * 256))
-    address += 3
-'''
-for i in range(max([max(sbase), max(soverdub)])+1):
-    index.append(i)
-
-#print(hex(address))
-
-
 # dump interpretted data
 print("Base               : Overdub")
 for i in range(len(base)):
     if base[i] == overdub[i] or overdub[i] == 0:
-        print("%1.8f 0x%4.4X -> 0x%6.6X" % (i/len(base), base[i],  index[base[i]]))
+        print("%1.8f 0x%4.4X -> 0x%8.8X" % (i/(len(base)+1), base[i], base[i]))
+        #print("%1.8f 0x%4.4X -> 0x%8.8X" % (i/(len(base)+1), base[i],  \
+        #        ((base[i] >> 3 ) * 0x1000) + 0x086C0000))
     else:
-        print("%1.8f 0x%4.4X -> 0x%6.6X : 0x%4.4X -> 0x%6.6X" % (i/len(base), \
-                base[i], index[base[i]], overdub[i], index[overdub[i]]))
+        print("%1.8f 0x%4.4X -> 0x%4.4X : 0x%4.4X -> 0x%4.4X" % (i/(len(base)+1), \
+                base[i], base[i], overdub[i], overdub[i]))
 
