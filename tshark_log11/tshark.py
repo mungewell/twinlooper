3bb5fb2ae8bfe26e90351612620649b10d28cf7e
Simon Wood <simon@mungewell.org>
2023-02-21 18:12:24 -0700

    Tshark_log11 - delet

---
+++ tshark_log11/tshark.py
@@ -0,0 +1,21 @@
+# Logs processed with:
+#
+# $ tshark -r log.pcapng -2 -R 'sysex' -T fields   -e usb.src -e usb.dst -e usbaudio.sysex.reassembled.data > log.txt
+
+import twinlooper
+from hexdump import hexdump
+
+pedal = twinlooper.twinlooper()
+
+dump = open("log.txt", "r")
+lines = dump.readlines()
+  
+# Strips the newline character
+for line in lines:
+    text = line[37:-4:].replace(":", "" )
+    test = bytes.fromhex(text)
+
+    print(line[0:80])
+    print(hexdump(pedal.unpack(test, 65)[:64]))
+    print("--")
+    
